3306af3f569974394af23a9e3ceb2d91d21f2555
aryaman-27 <chaudhary.aryaman07@gmail.com>
2022-03-28 13:36:49 +0530

    Working code with jerkey up movement

--- bird_code.py
+++ bird_code.py
@@ -1,4 +1,5 @@
 import pygame
+from  time import sleep
 
 class Bird:
     def __init__(self, fb_game):
@@ -6,15 +7,24 @@ class Bird:
         self.screen_rect = self.screen.get_rect()
         self.image = pygame.image.load('bird_image.png')
         self.image_rect = self.image.get_rect()
-        self.moving_up = False
+        self.bird_direction = 0
+
+        self.location = [200, 200]
 
     def draw_bird(self):
-        self.screen.blit(self.image, (200, 200))
+        self.screen.blit(self.image, self.location)
 
     def move_bird(self):
-        if self.moving_up:
+        if self.bird_direction == 1:
             for n in range(20):
-                self.image_rect.x -= 1
+                self.location[1] -= 1
+                sleep(0.01)
+                self.draw_bird()
+                pygame.display.flip()
+
+        else:
+            self.location[1] += 1
+
 
 
 
