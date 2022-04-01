c7f5a007cb1200c9f32c826f85700eaae3fd167f
aryaman-27 <chaudhary.aryaman07@gmail.com>
2022-04-01 20:14:57 +0530

    gap between the pillar is created

--- pillar_code.py
+++ pillar_code.py
@@ -1,16 +1,26 @@
 import pygame
+import  random
 from time import sleep
 
 class Pillar:
     def __init__(self, fb_game):
         self.screen = fb_game.screen
         self.screen_rect = self.screen.get_rect()
-        self.rect = pygame.Rect(0, 0, 20, 600)
-        self.rect.right = 800
+        self.upper_pillar_y = random.randint(0, 770)
+        self.lower_pillar_y = self.upper_pillar_y + 30
+        self.upper_rect = pygame.Rect(0, 0, 20, self.upper_pillar_y)
+        self.lower_rect = pygame.Rect(0, 0, 20, 600 - self.lower_pillar_y)
+        self.upper_rect.right = 800
+        self.lower_rect.right = 800
+        self.lower_rect.top = self.lower_pillar_y
         self.color = (255, 255, 0)
 
     def update(self):
-        self.rect.x -= 1
+        self.upper_rect.x -= 1
+        self.lower_rect.x -= 1
 
     def draw_pillar(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color, self.upper_rect)
+        pygame.draw.rect(self.screen, self.color, self.lower_rect)
+
+
